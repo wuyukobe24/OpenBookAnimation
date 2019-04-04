9672455d4a50a80ac612319f1f99944ff78e8ce0
wangxueqi <wangxueqi1@100tal.com>
2019-04-04 14:19:28 +0800

    Initial Commit

---
+++ PageAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageAnimation
+//
+//  Created by WXQ on 2019/4/4.
+//  Copyright © 2019 WXQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PageAnimation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PageAnimation
+//
+//  Created by WXQ on 2019/4/4.
+//  Copyright © 2019 WXQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
